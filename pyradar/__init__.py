1325d7396d6bf62ecfa4035ed47937072b21b93d
Wangzhaoze <wangzhaoze@outlook.com>
2024-09-25 22:52:59 +0200

    init commit

---
+++ pyradar/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Time    : xxxx-xx-xx
+# @Author  : <name>
+# @Site    : https://github.com/Wangzhaoze/<repo>
+# @File    : <file name>.py
+# @IDE     : vscode
+
+"""Import Scripts."""
--- shell/update_env_config.sh
+++ shell/update_env_config.sh
@@ -1,4 +1,3 @@
 conda activate pyradar
 conda env export --no-builds > environment.yml
 conda list --export | grep -v '^#' | grep 'pypi' > requirements.txt
-
